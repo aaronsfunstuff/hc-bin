3d0ba899a5a8e2cbfd45c9e90a771b3d1bb7d867
aaronsfunstuff <135081747+aaronsfunstuff@users.noreply.github.com>
2024-08-05 15:17:22 -0700

    Add files via upload

---
+++ The Bin! Copy/sketch.ino
@@ -0,0 +1,11 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define BUTTON_PIN 16
+#define BUZZER_PIN 18
+#define LED_PIN 19
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
